a3619a100fd824e61d54b419b88355ae7e9590c9
Stybyk <Topek.man@gmail.com>
2020-10-21 15:12:14 +0200

    Update main.py
    Code is more robust, verify if database is up.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import time
 import json
 import subprocess
@@ -6,18 +5,16 @@ import subprocess
 from influxdb import InfluxDBClient
 
 # InfluxDB Settings
-DB_ADDRESS = '10.10.10.10'
+DB_ADDRESS = 'db_hostname.network'
 DB_PORT = 8086
-DB_USER = 'user'
-DB_PASSWORD = 'passwd'
+DB_USER = 'db_username'
+DB_PASSWORD = 'db_password'
 DB_DATABASE = 'speedtest_db'
-
- # Modify hostname to identify source of the data as you need
-
+DB_RETRY_INVERVAL = 60 # Time before retrying a failed data upload.
 HOST_SRC = 'hostname'
 
 # Speedtest Settings
-TEST_INTERVAL = 3600  # Time between tests (in seconds).
+TEST_INTERVAL = 1800  # Time between tests (in seconds).
 TEST_FAIL_INTERVAL = 60  # Time before retrying a failed Speedtest (in seconds).
 
 influxdb_client = InfluxDBClient(
@@ -25,13 +22,17 @@ influxdb_client = InfluxDBClient(
 
 
 def init_db():
-    databases = influxdb_client.get_list_database()
-
-    if len(list(filter(lambda x: x['name'] == DB_DATABASE, databases))) == 0:
-        influxdb_client.create_database(
-            DB_DATABASE)  # Create if does not exist.
+    try:
+        databases = influxdb_client.get_list_database()
+    except:
+        print("Error: Unable to get list of databases")
+        raise RuntimeError("No DB connection") from error
     else:
-        influxdb_client.switch_database(DB_DATABASE)  # Switch to if does exist.
+        if len(list(filter(lambda x: x['name'] == DB_DATABASE, databases))) == 0:
+            influxdb_client.create_database(
+                DB_DATABASE)  # Create if does not exist.
+        else:
+            influxdb_client.switch_database(DB_DATABASE)  # Switch to if does exist.
 
 
 def format_for_influx(cliout):
@@ -42,14 +43,13 @@ def format_for_influx(cliout):
             'measurement': 'ping',
             'time': data['timestamp'],
             'fields': {
-                'jitter': data['ping']['jitter'],
-                'latency': data['ping']['latency']
+                'jitter': float(data['ping']['jitter']),
+                'latency': float(data['ping']['latency'])
             },
             'tags':{
                 'host':HOST_SRC
 
            }
-            
         },
         {
             'measurement': 'download',
@@ -83,7 +83,7 @@ def format_for_influx(cliout):
             'measurement': 'packetLoss',
             'time': data['timestamp'],
             'fields': {
-                'packetLoss': data['packetLoss'] + 0.0001,  # Preventing influxdb crate packetloss as integer eg. 0.0% packetLoss = always 0 int 
+                'packetLoss': float(data['packetLoss'])
             },
             'tags':{
                 'host':HOST_SRC
@@ -91,25 +91,40 @@ def format_for_influx(cliout):
            }
         }
     ]
-
+            
     return influx_data
 
 
 def main():
-    init_db()  # Setup the database if it does not already exist.
-
+    db_initialized = False
+    
+    while(db_initialized == False): 
+        try:
+            init_db()  # Setup the database if it does not already exist.
+        except:
+            print("Error: DB initialization error")
+            time.sleep(DB_RETRY_INVERVAL)
+        else:
+            print("Info: DB initialization complete")
+            db_initialized = True
+        
+        
     while (1):  # Run a Speedtest and send the results to influxDB indefinitely.
         speedtest = subprocess.run(
             ["speedtest", "--accept-license", "--accept-gdpr", "-f", "json"], capture_output=True)
 
         if speedtest.returncode == 0:  # Speedtest was successful.
             data = format_for_influx(speedtest.stdout)
-            print("Speedtest Successful:")
-            if influxdb_client.write_points(data) == True:
-                print("Data written to DB successfully")
-                time.sleep(TEST_INTERVAL)
+            print("Info: Speedtest successful")
+            try:
+                if influxdb_client.write_points(data) == True:
+                    print("Info: Data written to DB successfully")
+                    time.sleep(TEST_INTERVAL)
+            except:
+                print("Error: Data write to DB failed")
+                time.sleep(TEST_FAIL_INTERVAL)
         else:  # Speedtest failed.
-            print("Speedtest Failed:")
+            print("Error: Speedtest failed")
             print(speedtest.stderr)
             print(speedtest.stdout)
             time.sleep(TEST_FAIL_INTERVAL)
